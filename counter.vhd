1c78a57bf6f332682b48894242f4a4ec60456873
Josh Jacobson <joshjacobson14@gmail.com>
2013-12-08 11:03:47 -0600

    Counter finished, untested

--- counter.vhd
+++ counter.vhd
@@ -7,12 +7,28 @@ use work.eecs361_gates.all;
 
 entity counter is
   port (
-    x   : in  std_logic_vector(n-1 downto 0);
-    y   : in  std_logic_vector(n-1 downto 0);
-    z   : out std_logic_vector(n-1 downto 0)
+    clk   : in  std_logic;
+    increment_enable   : in  std_logic;
+    old_value   : in std_logic_vector(31 downto 0);
+    new_value   : out std_logic_vector(31 downto 0)
   );
-end and_gate_n;
+end counter;
 
-architecture structural of and_gate_n is
-begin;
+architecture structural of counter is
+    
+    component rippleadder32 is
+       port (
+          x       : in  std_logic_vector(31 downto 0);
+          y       : in  std_logic_vector(31 downto 0);
+          c       : in  std_logic;
+          z       : out std_logic_vector(31 downto 0);
+          cout    : out std_logic
+       );
+    end component rippleadder32;
+    
+    signal incremented_value : std_logic_vector(31 downto 0);
+    signal carry : std_logic;
+begin
+    adder_map : rippleadder32 port map (old_value, "00000000000000000000000000000001", '0', incremented_value, carry);
+    mux_map: mux_32 port map (increment_enable, old_value, incremented_value, new_value);
 end structural;
